b29e21c9bd17d023f9c13b0046e57662cbbcd353
yash2324 <yg232004@gmail.com>
2024-07-02 03:00:06 +0530

    added shadcn and forms

--- app/(auth)/signin/components/LoginForm.tsx
+++ app/(auth)/signin/components/LoginForm.tsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 
-import Input from '@/components/Input'
-import { signIn, signOut } from 'next-auth/react';
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
+import { signIn, signOut } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function LoginForm() {
-
   useEffect(() => {
     signOut({
       redirect: false,
     });
-  }, [])
+  }, []);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,41 +30,53 @@ export default function LoginForm() {
     setLoading(true);
 
     const login = await signIn("credentials", {
-      email, password, redirect: false,
+      email,
+      password,
+      redirect: false,
     });
 
-    if(login?.ok){
+    if (login?.ok) {
       toast.success("Correct login");
       window.location.assign("/");
-    }
-    else if(login?.error){
+    } else if (login?.error) {
       toast.error(login?.error);
     }
 
     setLoading(false);
-  }
+  };
 
   return (
-    <div className='space-y-5 flex flex-col items-center'>
-      <Input 
-        label='Email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        disabled={loading}
-      />
-      <Input 
-        label='Password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        disabled={loading}
-        type='password'
-      />
-      <div
-        onClick={login}
-        className='px-10 py-3 bg-neutral-900 rounded-full text-white disabled:opacity-70 cursor-pointer'
-      >
-        Login
-      </div>
-    </div>  
-  )
+    <Card className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>
+          Enter your email and password to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="Email">Email</Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled={loading}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              disabled={loading}
+              type="password"
+            />
+          </div>
+          <Button type="submit" onClick={login} className="w-full">
+            Login
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
--- app/(auth)/signup/components/RegisterForm.tsx
+++ app/(auth)/signup/components/RegisterForm.tsx
@@ -1,66 +1,86 @@
-"use client"
+"use client";
 
-import Input from '@/components/Input';
-import axios from 'axios';
-import { signIn, signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
+import axios from "axios";
+import { signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function RegisterForm() {
+  useEffect(() => {
+    signOut({
+      redirect: false,
+    });
+  }, []);
 
-    useEffect(() => {
-      signOut({
-        redirect: false,
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const register = async () => {
+    setLoading(true);
+    try {
+      await axios.post("/api/register", {
+        email,
+        password,
       });
-    }, []);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-  
-    const [loading, setLoading] = useState(false);
+      toast.success("Successfully registered");
 
-    const router = useRouter();
-  
-    const register = async () => {
-      setLoading(true);
-      try{
-        await axios.post("/api/register", {
-          email, password
-        });
-
-        toast.success("Successfully registered");
-
-        router.push("/signin");
-      } catch(err: any){
-        console.log(err);
-        toast.error(err?.response?.data)
-      } finally{
-        setLoading(false);
-      }
+      router.push("/signin");
+    } catch (err: any) {
+      console.log(err);
+      toast.error(err?.response?.data);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-      <div className='space-y-5 flex flex-col items-center'>
-        <Input 
-          label='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          disabled={loading}
-        />
-        <Input 
-          label='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          disabled={loading}
-          type='password'
-        />
-        <div
-          onClick={register}
-          className='px-10 py-3 bg-neutral-900 rounded-full text-white disabled:opacity-70 cursor-pointer'
-        >
-          Register
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Signup</CardTitle>
+        <CardDescription>
+          Enter your email and password to create to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="Email">Email</Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled={loading}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              disabled={loading}
+              type="password"
+            />
+          </div>
+          <Button type="submit" onClick={register} className="w-full">
+            Sign Up
+          </Button>
         </div>
-      </div>  
-    )
+      </CardContent>
+    </Card>
+  );
 }
